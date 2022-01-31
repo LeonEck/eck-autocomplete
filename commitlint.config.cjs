8b2b6eea927e1878c93c9a51cc1d073ead1e0433
Leon Eck <eck.leon95@gmail.com>
2022-01-31 16:46:08 +0100

    chore: setup commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'test',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+  },
+};
