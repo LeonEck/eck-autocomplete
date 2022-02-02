6af6582200f2ac8d9167473f6abf73857c3af131
Leon Eck <eck.leon95@gmail.com>
2022-02-02 17:32:14 +0100

    build: setup publish

--- scripts/esbuild-library-build.mjs
+++ scripts/esbuild-library-build.mjs
@@ -1,4 +1,7 @@
 import esbuild from 'esbuild';
+import { rmSync } from 'fs';
 import { esbuildLibraryConfig } from './esbuild-library-config.mjs';
+
+rmSync(esbuildLibraryConfig.outdir, { recursive: true, force: true });
 
 esbuild.build(esbuildLibraryConfig).then(() => {});
