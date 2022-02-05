e5ebc3d0ab475c48463acffdabe0a88f86e5a307
Leon Eck <eck.leon95@gmail.com>
2022-02-05 16:09:12 +0100

    build: generate additional minified files

--- scripts/esbuild-library-build.mjs
+++ scripts/esbuild-library-build.mjs
@@ -1,7 +1,18 @@
 import esbuild from 'esbuild';
 import { rmSync } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { esbuildLibraryConfig } from './esbuild-library-config.mjs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 rmSync(esbuildLibraryConfig.outdir, { recursive: true, force: true });
 
-esbuild.build(esbuildLibraryConfig).then(() => {});
+esbuild.build(esbuildLibraryConfig).then(() => {
+  /**
+   * Run a second build for minified output.
+   */
+  esbuildLibraryConfig.minify = true;
+  esbuildLibraryConfig.outdir = resolve(__dirname, '../dist/min');
+  esbuild.build(esbuildLibraryConfig).then(() => {});
+});
