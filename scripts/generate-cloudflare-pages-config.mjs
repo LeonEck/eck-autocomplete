4b1ec0317947a98f5682801a0bbffcfe794d94e6
Leon Eck <eck.leon95@gmail.com>
2022-02-05 17:28:26 +0100

    build: add cache headers to showcase

---
+++ scripts/generate-cloudflare-pages-config.mjs
@@ -0,0 +1,17 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const headers = `
+/*
+  X-Frame-Options: DENY
+  X-Content-Type-Options: nosniff
+  Referrer-Policy: no-referrer
+/assets/*.js
+  Cache-Control: public, max-age=31536000, s-max-age=31536000
+  Vary: Accept-Encoding
+`;
+
+writeFileSync(resolve(__dirname, '../www/_headers'), headers.trim());
