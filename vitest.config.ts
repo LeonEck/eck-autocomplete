24c0f6426314cfc0d7ff8cdd4f88f0d33ac20ae4
Leon Eck <eck.leon95@gmail.com>
2022-02-27 08:54:44 +0100

    test(unit): exclude build-artifacts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   test: {
     environment: 'happy-dom',
+    exclude: [...(configDefaults.exclude as string[]), 'build-artifacts/**'],
   },
 });
