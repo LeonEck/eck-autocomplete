0b296b4d36c27ef2c3e64d620622827fbdcfeeeb
Leon Eck <eck.leon95@gmail.com>
2022-03-26 14:05:17 +0100

    test: focus before checking highlighting

--- e2e/highlight-first-option/highlight-first-option.spec.ts
+++ e2e/highlight-first-option/highlight-first-option.spec.ts
@@ -8,6 +8,7 @@ test.describe('highlight-first-option', () => {
   test('should not highlight anything when attribute is not present', async ({
     page,
   }) => {
+    await page.locator('#input1').focus();
     const attribute = await page
       .locator('#complete1 > eck-autocomplete-option')
       .first()
@@ -18,6 +19,7 @@ test.describe('highlight-first-option', () => {
   test('should highlight first option when attribute is set to the string "null"', async ({
     page,
   }) => {
+    await page.locator('#input2').focus();
     const attribute = await page
       .locator('#complete2 > eck-autocomplete-option')
       .first()
@@ -28,6 +30,7 @@ test.describe('highlight-first-option', () => {
   test('should highlight first option when attribute is set to an empty string', async ({
     page,
   }) => {
+    await page.locator('#input3').focus();
     const attribute = await page
       .locator('#complete3 > eck-autocomplete-option')
       .first()
@@ -38,6 +41,7 @@ test.describe('highlight-first-option', () => {
   test('should not highlight first option when attribute is set to the string "false"', async ({
     page,
   }) => {
+    await page.locator('#input4').focus();
     const attribute = await page
       .locator('#complete4 > eck-autocomplete-option')
       .first()
@@ -48,6 +52,7 @@ test.describe('highlight-first-option', () => {
   test('should highlight first option when attribute is set to the string "true"', async ({
     page,
   }) => {
+    await page.locator('#input5').focus();
     const attribute = await page
       .locator('#complete5 > eck-autocomplete-option')
       .first()
@@ -58,6 +63,7 @@ test.describe('highlight-first-option', () => {
   test('should highlight first option when attribute is set to an arbitrary string', async ({
     page,
   }) => {
+    await page.locator('#input6').focus();
     const attribute = await page
       .locator('#complete6 > eck-autocomplete-option')
       .first()
@@ -68,6 +74,7 @@ test.describe('highlight-first-option', () => {
   test('should highlight first option when attribute is set as a boolean attribute', async ({
     page,
   }) => {
+    await page.locator('#input7').focus();
     const attribute = await page
       .locator('#complete7 > eck-autocomplete-option')
       .first()
