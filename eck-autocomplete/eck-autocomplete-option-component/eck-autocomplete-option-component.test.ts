67b36dfea575fbb1c6d8b1f6e4168ee61f70860f
Leon Eck <eck.leon95@gmail.com>
2022-05-11 18:46:48 +0200

    test: add DOM test with vitest

---
+++ eck-autocomplete/eck-autocomplete-option-component/eck-autocomplete-option-component.test.ts
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import {
+  ECK_AUTOCOMPLETE_OPTION_TAG_NAME,
+  EckAutocompleteOption,
+  EckAutocompleteOptionSelectEvent,
+} from './eck-autocomplete-option-component';
+
+describe('eck-autocomplete', () => {
+  beforeEach(() => {
+    if (customElements.get(ECK_AUTOCOMPLETE_OPTION_TAG_NAME) === undefined) {
+      customElements.define(
+        ECK_AUTOCOMPLETE_OPTION_TAG_NAME,
+        EckAutocompleteOption
+      );
+    }
+  });
+
+  it('should get defined', () => {
+    expect(customElements.get(ECK_AUTOCOMPLETE_OPTION_TAG_NAME)).toBeTruthy();
+  });
+
+  it('should fire eck-autocomplete-option-selected on host click', () => {
+    const option = document.createElement(ECK_AUTOCOMPLETE_OPTION_TAG_NAME);
+    option.innerText = 'Label Text';
+    document.body.appendChild(option);
+    option.addEventListener('eck-autocomplete-option-selected', ((
+      value: CustomEvent<EckAutocompleteOptionSelectEvent>
+    ) => {
+      expect(value.detail.label).toBe(option.innerText);
+    }) as EventListener);
+    option.click();
+  });
+});
