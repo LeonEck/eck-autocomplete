9d25c74408c2d80435358f0d8b9cbd41e4549802
Leon Eck <eck.leon95@gmail.com>
2022-06-17 16:57:46 +0200

    chore: run prettier

--- e2e/interaction/combinations.spec.ts
+++ e2e/interaction/combinations.spec.ts
@@ -4,31 +4,26 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/e2e/interaction/');
 });
 
-test.describe(
-  'interaction-highlight-first-option-and-select-highlighted-option-deactivated',
-  () => {
-    test('input should stay clear when opening panel', async ({ page }) => {
-      await page.focus('#input4');
-      await expect(page.locator('#input4')).toHaveValue('');
-      await expect(page.locator('#complete4')).toBeVisible();
-    });
+test.describe('interaction-highlight-first-option-and-select-highlighted-option-deactivated', () => {
+  test('input should stay clear when opening panel', async ({ page }) => {
+    await page.focus('#input4');
+    await expect(page.locator('#input4')).toHaveValue('');
+    await expect(page.locator('#complete4')).toBeVisible();
+  });
 
-    test('input should not get second value when pressing ArrowDown', async ({
-      page,
-    }) => {
-      await page.focus('#input4');
-      await page.keyboard.press('ArrowDown');
-      await expect(page.locator('#input4')).toHaveValue('');
-      await expect(page.locator('#complete4')).toBeVisible();
-    });
+  test('input should not get second value when pressing ArrowDown', async ({
+    page,
+  }) => {
+    await page.focus('#input4');
+    await page.keyboard.press('ArrowDown');
+    await expect(page.locator('#input4')).toHaveValue('');
+    await expect(page.locator('#complete4')).toBeVisible();
+  });
 
-    test('input should get first value when pressing Enter', async ({
-      page,
-    }) => {
-      await page.focus('#input4');
-      await page.keyboard.press('Enter');
-      await expect(page.locator('#input4')).toHaveValue('One');
-      await expect(page.locator('#complete4')).not.toBeVisible();
-    });
-  }
-);
+  test('input should get first value when pressing Enter', async ({ page }) => {
+    await page.focus('#input4');
+    await page.keyboard.press('Enter');
+    await expect(page.locator('#input4')).toHaveValue('One');
+    await expect(page.locator('#complete4')).not.toBeVisible();
+  });
+});
