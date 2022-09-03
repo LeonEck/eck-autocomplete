791fdf34f29008868fb6c40bf94a072d82a21424
Leon Eck <eck.leon95@gmail.com>
2022-09-03 13:58:39 +0200

    test: add test for ResizeObserver error

---
+++ e2e/animation/animation.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/e2e/animation/');
+});
+
+test.describe('animation', () => {
+  test('input should stay clear when opening panel', async ({
+    page,
+    browserName,
+  }) => {
+    await page.focus('#input1');
+
+    /**
+     * https://stackoverflow.com/a/45853382
+     */
+    if (browserName === 'webkit') {
+      await expect(page.locator('#errors')).toContainText('Script error');
+    } else {
+      await expect(page.locator('#errors')).toContainText('ResizeObserver');
+    }
+  });
+});
